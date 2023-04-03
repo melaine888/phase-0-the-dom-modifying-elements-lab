c6d07034f986832a2c86abec12a0b74fb84cda0a
Melaine Kanana <melaine.kanana@student.moringaschool.com>
2023-04-03 10:26:59 +0300

    DONE WITH ASSIGNMENT

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+const main = document.getElementById('main')
+
+main.remove();
+
+
+var newHeader = document.createElement('h1')
+
+newHeader.id = 'victory'
+newHeader.innerHTML = 'YOUR-NAME is the champion'
+newHeader.append();
+
